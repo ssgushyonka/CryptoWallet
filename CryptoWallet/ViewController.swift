b2f2d8bfbf28afc79fb14775276ab06eb8f004b2
ssgushyonka <orangeee04@gmail.com>
2025-05-19 13:09:16 +0300

    Initial Commit

---
+++ CryptoWallet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CryptoWallet
+//
+//  Created by Элина Борисова on 19.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
